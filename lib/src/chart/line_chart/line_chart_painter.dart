0e5b509d15c3e7cdeda3749c5110a005c18b64b4
Slava <vyacheslavmokrousov@gmail.com>
2020-11-26 16:06:53 +0300

    fix for scale graphs

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -344,7 +344,8 @@ class LineChartPainter extends AxisChartPainter<LineChartData>
     for (int i = 0; i < barData.showingIndicators.length; i++) {
       final TouchedSpotIndicatorData indicatorData = indicatorsData[i];
       final int index = barData.showingIndicators[i];
-      final FlSpot spot = barData.spots[index];
+      final FlSpot spot =
+          barData.spots[index < data.maxY ? data.maxY.toInt() : index];
 
       if (indicatorData == null) {
         continue;
